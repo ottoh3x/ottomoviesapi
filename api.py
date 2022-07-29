705f42f82d069c0549d6d118f6101e8001aebfa6
otto <otmankhess@gmail.com>
2022-07-29 19:03:25 +0100

    first

--- api.py
+++ api.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+import uvicorn
+
 from MoviesApi import *
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
@@ -37,3 +51,7 @@ def popular_tv():
 def popular_movies():
     popular_movies = HomeMoviesApi.popularMovies(None)
     return popular_movies
+
+
+if __name__== "__main__":
+   uvicorn.run(app, host="127.0.0.1", port=8080)
