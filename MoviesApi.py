7c0ea1d0bbd8dba9dbf1c359b9db75bf1b471338
otto <otmankhess@gmail.com>
2022-08-29 14:47:18 +0100

    all

--- MoviesApi.py
+++ MoviesApi.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
+import aiohttp
+import asyncio
+
 import requests
 
 
@@ -7,15 +10,20 @@ headers = {
 }
 
 
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as res:
+            r = await res.text()
+            return r
+
 class HomeMoviesApi():
     def __init__(self, movie_id):
         self.movie_id = movie_id
 
     def trendingMovies(self):
         links = []
-        url = "https://lookmoviess.com"
-        r = requests.get(url).text
-        soup = BeautifulSoup(r, 'html.parser')
+        results = asyncio.run(fetch("https://lookmoviess.com/"))
+        soup = BeautifulSoup(results, 'html.parser')
         trending_movies = soup.find(id="trending-movies")
         for i in trending_movies.find_all('div', 'flw-item'):
             image = i.img['data-src']
@@ -157,13 +165,59 @@ class HomeMoviesApi():
         return links
 
     def moviesEpisode(movie_id):
+        data = []
         links = {}
         url = f"https://lookmoviess.com/movie/{movie_id}"
         r = requests.get(url).text
         soup = BeautifulSoup(r, "html.parser")
         iframe = soup.find(id="iframe-embed")['src']
+        details = soup.find('div',"detail_page-infor")
+        title = details.find("h2","heading-name").text
+        imdb = details.find('button','btn-imdb').text.replace(' ',"").replace("IMDB:",'')
+        trailer = soup.find(id="iframe-trailer")['data-src']
+        image = details.find('img')['src']
+        description = details.find('div','description').text.strip()
+        released = details.find_all('div','row-line')[0].text.replace(" ","").replace("Released:","")
+        released = released.strip()
+        duration = details.find_all('div','row-line')[3].text.replace(" ","").replace("Duration:","")
+        duration = duration.strip()
+        links['title'] = title
+        links['imdb'] = imdb
+        links['trailer'] = trailer
+        links['image'] = image
+        links['description'] = description
+        links['released'] = released
+        links['duration'] = duration
         links['iframe'] = iframe
-        return links
+        data.append(links)
+        genre = details.find_all('div','row-line')[1]
+        for x in genre.find_all('a'):
+            url = x['href']
+            genre = x['title']
+            data.append({"genres" :{"url": url, "genre": genre}})
 
-a = HomeMoviesApi.moviesEpisode(movie_id="the-innocents-85632")
-print(a)
+        casts = details.find_all('div','row-line')[2]
+        for x in casts.find_all('a'):
+            url = x['href']
+            title = x['title']
+            data.append({"casts" :{"url": url, "title": title}})
+        
+        country = details.find_all('div','row-line')[4]
+        for x in country.find_all('a'):
+            url = x['href']
+            country = x['title']
+            data.append({"countries" :{"url": url, "country": country}})
+        production = details.find_all('div','row-line')[5]
+        for x in production.find_all('a'):
+            url = x['href']
+            title = x['title']
+            data.append({"productions" :{"url": url, "title": title}})
+
+        
+
+        
+
+        return data
+
+movie = HomeMoviesApi.moviesEpisode(movie_id="the-innocents-85632")
+print(movie)
